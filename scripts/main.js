8fd8c0b9cf503da8d7bd56d195125945c347115c
kevinelong <kevinelong@gmail.com>
2024-06-05 08:09:24 -0700

    begin cart

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,35 @@ function getTitle(name){
     div.innerText = name;
     return div;
 }
+function getPrice(amount){
+    const div = document.createElement("div");
+    div.classList.add("amount");
+    div.innerText = "$" + amount.toFixed(2);
+    return div;
+}
 
+function getImages(images){
+    const img = document.createElement("img");
+    img.classList.add("image");
+    const image = images[0]; //Use First. TODO find and use "default"
+    img.src = image.path;
+    img.alt = image.name;
+    return img;
+}
+function getBuyButton(product){
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.innerText = "Add to Cart"
+    button.dataset.product = encodeURI(JSON.stringify(product));
+    return button;
+}
 function getProduct(product){
     const div = document.createElement("div");
     div.classList.add("card");
+    div.appendChild(getBuyButton(product));
     div.appendChild(getTitle(product.name));
-    // div.appendChild(getImages(product.images));
+    div.appendChild(getPrice(product.price));
+    div.appendChild(getImages(product.images));
     div.appendChild(getDescription(product.desc));
     return div;
 }
@@ -24,11 +47,10 @@ function getProduct(product){
 
 document.addEventListener("DOMContentLoaded", async e=>{
 
-    products.innerHTML = "???";
+    products.innerHTML = "";
 
     const response = await fetch("./data/products.json");//GET
     const data = await response.json();
     data.forEach(p => products.appendChild(getProduct(p)));
-
 
 });//end loaded
