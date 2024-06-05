e1d0d933a7daab1f9ee6db16e80d798fa41e4282
kevinelong <kevinelong@gmail.com>
2024-06-05 08:48:34 -0700

    cart

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,25 @@
-function getDescription(desc){
+function getDescription(desc) {
     const div = document.createElement("div");
     div.classList.add("desc");
     div.innerText = desc;
     return div;
 }
 
-function getTitle(name){
+function getTitle(name) {
     const div = document.createElement("div");
     div.classList.add("title");
     div.innerText = name;
     return div;
 }
-function getPrice(amount){
+
+function getPrice(amount) {
     const div = document.createElement("div");
     div.classList.add("amount");
     div.innerText = "$" + amount.toFixed(2);
     return div;
 }
 
-function getImages(images){
+function getImages(images) {
     const img = document.createElement("img");
     img.classList.add("image");
     const image = images[0]; //Use First. TODO find and use "default"
@@ -26,14 +27,17 @@ function getImages(images){
     img.alt = image.name;
     return img;
 }
-function getBuyButton(product){
+
+function getBuyButton(product) {
     const button = document.createElement("button");
     button.classList.add("btn");
     button.innerText = "Add to Cart"
     button.dataset.product = encodeURI(JSON.stringify(product));
+    button.addEventListener("click", e => addItem(product));
     return button;
 }
-function getProduct(product){
+
+function getProduct(product) {
     const div = document.createElement("div");
     div.classList.add("card");
     div.appendChild(getBuyButton(product));
@@ -44,8 +48,40 @@ function getProduct(product){
     return div;
 }
 
-
-document.addEventListener("DOMContentLoaded", async e=>{
+function getLine(item) {
+    const ext = item.qty * item.product.price;
+    return `
+        <tr>
+            <td class="qty"><input type="number" value="${item.qty}"></td>
+            <td class="name">    ${item.product.name}</td>
+            <td class="desc">    ${item.product.desc}</td>
+            <td class="price"> $ ${item.product.price.toFixed(2)}</td>
+            <td class="ext">   $ ${ext.toFixed(2)}</td>
+            <td class="actions"><span class="remove">remove</span></td>
+            
+        </tr>
+    `;
+}
+function displayCart() {
+    cart.innerHTML = `
+    <table>
+        <tr>
+            <th>QTY</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>EXT PRICE</th>
+            <th>Actions</th>
+        </tr>
+        ${cartItems.map(getLine).join("")}
+    </table>`
+}
+let cartItems = [];
+function addItem(product, qty = 1) {
+    cartItems.push({ "qty": qty, "product": product }) //create object literal
+    displayCart();
+}
+document.addEventListener("DOMContentLoaded", async e => {
 
     products.innerHTML = "";
 
